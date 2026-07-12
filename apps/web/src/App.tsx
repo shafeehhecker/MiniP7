17143d150a150575e71e5fdc4fafc512611d625c
Muhammed Shafeeh <98475872+shafeehhecker@users.noreply.github.com>
2026-07-12 08:12:52 +0300

    Create App.tsx
    Root component, router + protected routes

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { MainLayout } from "./components/layout/MainLayout";
+import { LoginPage } from "./pages/LoginPage";
+import { SignupPage } from "./pages/SignupPage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { ProjectPage } from "./pages/ProjectPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            element={
+              <ProtectedRoute>
+                <MainLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/projects/:projectId" element={<ProjectPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
